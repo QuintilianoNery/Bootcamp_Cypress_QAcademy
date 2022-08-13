3ecfaa71d9466269f11314c2fabd2c0ae80fac86
Quintiliano <quintiliano1995@gmail.com>
2022-08-13 01:04:50 -0300

    Testes criados usando massa de dados

--- qtruck/tests/cypress/e2e/login.cy.js
+++ qtruck/tests/cypress/e2e/login.cy.js
@@ -11,18 +11,41 @@ describe('Login', () => {
   });
   //Login com sucesso
   it('Must login successfully', () => {
-    cy.get('input[name="instagram"]').type(perfil.instagram, { log: false });
-    cy.get('input[name="password"]').type(perfil.senha, { log: false });
+    cy.get('input[name="instagram"]').type(perfil.loginValido.instagram, { log: false });
+    cy.get('input[name="password"]').type(perfil.loginValido.senha, { log: false });
     cy.get('button[type="submit"]').click();
 
     cy.get('h2')
       .should('be.visible')
       .should('have.text', 'Escolha um food truck no mapa')
-      
+
     cy.get('p[class="logged-user"]')
       .should('be.visible')
       .should('have.text', 'Olá, ' + perfil.nome);
 
   })
+
+  // Não deve locar com senha inválida
+  it('Must not login with incorrect password', () => {
+    cy.get('input[name="instagram"]').type(perfil.loginValido.instagram, { log: false });
+    cy.get('input[name="password"]').type(perfil.loginInvalido.senhaInvalida, { log: false });
+    cy.get('button[type="submit"]').click();
+
+    cy.get('div[class="swal2-html-container"]')
+      .should('be.visible')
+      .should('have.text', 'Credenciais inválidas, tente novamente!');
+
+  });
+
+  it('Must not login with incorrect instagram', () => {
+    cy.get('input[name="instagram"]').type(perfil.loginInvalido.instagramInvalido, { log: false });
+    cy.get('input[name="password"]').type(perfil.loginValido.senha, { log: false });
+    cy.get('button[type="submit"]').click();
+
+    cy.get('div[class="swal2-html-container"]')
+      .should('be.visible')
+      .should('have.text', 'Credenciais inválidas, tente novamente!');
+
+  });
 
 })
