cadb532aec3c468affa1d64790080613647e04b5
Quintiliano <quintiliano1995@gmail.com>
2022-08-13 01:49:52 -0300

    Finalizar aula 02

--- qtruck/tests/cypress/e2e/login.cy.js
+++ qtruck/tests/cypress/e2e/login.cy.js
@@ -7,13 +7,14 @@ describe('Login', () => {
   });
 
   //Login com sucesso
+  //Utilizando Custom Commands, apenas se passa os valores de cada comando personalizado
+  //nesse caso estou passando os valores da minha massa de dados que está em fixtures
   it('Must login successfully', () => {
     cy.login(perfil.loginValido.instagram, perfil.loginValido.senha);
     cy.loggedUser(perfil.nome);
 
   })
 
-  // Não deve locar com senha inválida
   it('Must not login with incorrect password', () => {
     cy.login(perfil.loginValido.instagram, perfil.loginInvalido.senhaInvalida);
     cy.modalHaveText('Credenciais inválidas, tente novamente!')
